ff7b015263ef4288d98f038ea1ce7059c6ff696f
Katya Samsonova <89536093+samsonovak8@users.noreply.github.com>
2023-04-21 00:02:03 +0300

    Update f2.cpp
    1) пробелы done
    2) вынесла Init()
    3) совместила Sqrt2 и Sqrt3
    4) вынесла константы в отдельный класс

--- f2.cpp
+++ f2.cpp
@@ -1,128 +1,146 @@
-//Meisell-Lehmer
-#include <cstdio>
+// Meisell-Lehmer
 #include <cmath>
+#include <cstdio>
 #include <iostream>
 
-const int64_t kN = 5e6 + 2;
-bool np[kN];
-int64_t prime[kN], pi[kN];
-int64_t GetPrime()
-{
-    int64_t cnt = 0;
-    np[0] = np[1] = true;
-    pi[0] = pi[1] = 0;
-    for(int64_t i = 2; i< kN; ++i) {
-        if(!np[i])  {
-            prime[++cnt] = i;
-        }
-        pi[i] = cnt;
+class Constant {
+ public:
+  static const int64_t kN = 5e6 + 2;
+  static bool np[kN];
+  static int64_t prime[kN];
+  static int64_t pi[kN];
+  static const int64_t kM = 7;
+  static const int64_t kPM = 2 * 3 * 5 * 7 * 11 * 13 * 17;
+  static int64_t phi[kPM + 1][kM + 1];
+  static int64_t sz[kM + 1];
+};
 
-        for(int64_t j = 1; j <= cnt && i * prime[j] < kN; ++j) {
-            np[i * prime[j]] = true;
-            if(i % prime[j] == 0) {
-                break;
-            }
-        }
+const int64_t Constant::kN;
+bool Constant::np[Constant::kN];
+int64_t Constant::prime[Constant::kN];
+int64_t Constant::pi[Constant::kN];
+const int64_t Constant::kM;
+const int64_t Constant::kPM;
+int64_t Constant::phi[Constant::kPM + 1][Constant::kM + 1];
+int64_t Constant::sz[Constant::kM + 1];
+
+int64_t GetPrime() {
+  int64_t cnt = 0;
+  Constant::np[0] = Constant::np[1] = true;
+  Constant::pi[0] = Constant::pi[1] = 0;
+  for (int64_t i = 2; i < Constant::kN; ++i) {
+    if (!Constant::np[i]) {
+      Constant::prime[++cnt] = i;
     }
-    return cnt;
-}
+    Constant::pi[i] = cnt;
 
-const int64_t kM= 7;
-const int64_t kPM= 2 * 3 * 5 * 7 * 11 * 13 * 17;
-int64_t phi[kPM+ 1][kM+ 1];
-int64_t sz[kM+ 1];
+    for (int64_t j = 1; j <= cnt && i * Constant::prime[j] < Constant::kN;
+         ++j) {
+      Constant::np[i * Constant::prime[j]] = true;
+      if (i % Constant::prime[j] == 0) {
+        break;
+      }
+    }
+  }
+  return cnt;
+}
 
 void Init() {
-    GetPrime();
-    sz[0] = 1;
-    for(int64_t i = 0; i <= kPM; ++i) {
-        phi[i][0] = i;
+  GetPrime();
+  Constant::sz[0] = 1;
+  for (int64_t i = 0; i <= Constant::kPM; ++i) {
+    Constant::phi[i][0] = i;
+  }
+  for (int64_t i = 1; i <= Constant::kM; ++i) {
+    Constant::sz[i] = Constant::prime[i] * Constant::sz[i - 1];
+    for (int64_t j = 1; j <= Constant::kPM; ++j) {
+      Constant::phi[j][i] = Constant::phi[j][i - 1] -
+                            Constant::phi[j / Constant::prime[i]][i - 1];
     }
-    for(int64_t i = 1; i <= kM; ++i) {
-        sz[i] = prime[i] * sz[i - 1];
-        for(int64_t j = 1; j <= kPM; ++j) {
-            phi[j][i] = phi[j][i - 1] - phi[j / prime[i]][i - 1];
-        }
-    }
+  }
 }
 
-int64_t Sqrt2(int64_t x)
-{
-    auto r = static_cast<int64_t>(sqrt(static_cast<double>(x) - 0.1));
-    while(r * r <= x) {
-        ++r;
+int64_t SqrtUniversal(int64_t x, int64_t degree) {
+  int64_t r = 0;
+  if (degree == 2) {
+    r = static_cast<int64_t>(sqrt(static_cast<double>(x) - 0.1));
+    while (r * r <= x) {
+      ++r;
     }
-    return r - 1;
-}
-
-int64_t Sqrt3(int64_t x) {
-    auto r = static_cast<int64_t>(cbrt(static_cast<double>(x) - 0.1));
-    while(r * r * r <= x) {
-        ++r;
+  } else {
+    r = static_cast<int64_t>(cbrt(static_cast<double>(x) - 0.1));
+    while (r * r * r <= x) {
+      ++r;
     }
-    return r - 1;
+  }
+  return r - 1;
 }
 
 int64_t GetPhi(int64_t x, int64_t s) {
-    if(s == 0) {
-        return x;
-    } 
-    if(s <= kM) {
-        return phi[x % sz[s]][s] + (x / sz[s]) * phi[sz[s]][s];
+  if (s == 0) {
+    return x;
+  }
+  if (s <= Constant::kM) {
+    return Constant::phi[x % Constant::sz[s]][s] +
+           (x / Constant::sz[s]) * Constant::phi[Constant::sz[s]][s];
+  }
+  if (x <= Constant::prime[s] * Constant::prime[s]) {
+    return Constant::pi[x] - s + 1;
+  }
+  if (x <= Constant::prime[s] * Constant::prime[s] * Constant::prime[s] &&
+      x < Constant::kN) {
+    int64_t s2x = Constant::pi[SqrtUniversal(x, 2)];
+    int64_t ans = Constant::pi[x] - (s2x + s - 2) * (s2x - s + 1) / 2;
+    for (int64_t i = s + 1; i <= s2x; ++i) {
+      ans += Constant::pi[x / Constant::prime[i]];
     }
-    if(x <= prime[s]*prime[s]) {
-        return pi[x] - s + 1;
-    }
-    if(x <= prime[s]*prime[s]*prime[s] && x < kN) {
-        int64_t s2x = pi[Sqrt2(x)];
-        int64_t ans = pi[x] - (s2x +s - 2) * (s2x - s +1) / 2;
-        for(int64_t i = s + 1; i <= s2x; ++i) {
-            ans += pi[x / prime[i]];
-        }
-        return ans;
-    }
+    return ans;
+  }
 
-    return GetPhi(x, s - 1) - GetPhi(x / prime[s], s - 1);
+  return GetPhi(x, s - 1) - GetPhi(x / Constant::prime[s], s - 1);
 }
 
 int64_t GetPi(int64_t x) {
-    if(x < kN) {
-        return pi[x];
-    }
-    int64_t ans = GetPhi(x, pi[Sqrt3(x)]) + pi[Sqrt3(x)] - 1;
-    for(int64_t i = pi[Sqrt3(x)] + 1, ed = pi[Sqrt2(x)]; i <= ed; ++i) {
-        ans -= GetPi(x / prime[i]) - i + 1;
-    }
-    return ans;
+  if (x < Constant::kN) {
+    return Constant::pi[x];
+  }
+  int64_t ans = GetPhi(x, Constant::pi[SqrtUniversal(x, 3)]) +
+                Constant::pi[SqrtUniversal(x, 3)] - 1;
+  for (int64_t i = Constant::pi[SqrtUniversal(x, 3)] + 1,
+               ed = Constant::pi[SqrtUniversal(x, 2)];
+       i <= ed; ++i) {
+    ans -= GetPi(x / Constant::prime[i]) - i + 1;
+  }
+  return ans;
 }
 
 int64_t LehmerPi(int64_t x) {
-    if(x < kN) {
-        return pi[x];
+  Init();
+  if (x < Constant::kN) {
+    return Constant::pi[x];
+  }
+  int64_t a = LehmerPi(SqrtUniversal(SqrtUniversal(x, 2), 2));
+  int64_t b = LehmerPi(SqrtUniversal(x, 2));
+  int64_t c = LehmerPi(SqrtUniversal(x, 3));
+  int64_t sum =
+      GetPhi(x, a) + static_cast<int64_t>(b + a - 2) * (b - a + 1) / 2;
+  for (int64_t i = a + 1; i <= b; i++) {
+    int64_t w = x / Constant::prime[i];
+    sum -= LehmerPi(w);
+    if (i > c) {
+      continue;
     }
-    int64_t a = LehmerPi(Sqrt2(Sqrt2(x)));
-    int64_t b = LehmerPi(Sqrt2(x));
-    int64_t c = LehmerPi(Sqrt3(x));
-    int64_t sum = GetPhi(x, a) + static_cast<int64_t>(b +a - 2) * (b-a +1) / 2;
-    for (int64_t i =a +1; i <= b; i++) {
-        int64_t w = x / prime[i];
-        sum -= LehmerPi(w);
-        if (i > c) {
-            continue;
-        } 
-        int64_t lim = LehmerPi(Sqrt2(w));
-        for (int64_t j = i; j <= lim; j++) {
-            sum -= LehmerPi(w / prime[j]) - (j - 1);
-        }
+    int64_t lim = LehmerPi(SqrtUniversal(w, 2));
+    for (int64_t j = i; j <= lim; j++) {
+      sum -= LehmerPi(w / Constant::prime[j]) - (j - 1);
     }
-    return sum;
+  }
+  return sum;
 }
 
-int main()
-{
-    Init();
-    int64_t n = 0;
-    std::cin >> n;
-    std::cout << LehmerPi(n);
-    return 0;
+int main() {
+  int64_t n = 0;
+  std::cin >> n;
+  std::cout << LehmerPi(n);
+  return 0;
 }
