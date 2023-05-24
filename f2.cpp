d003943903830722592e1a00311024f47219a6aa
Katya Samsonova <89536093+samsonovak8@users.noreply.github.com>
2023-05-24 17:04:25 +0300

    Update f2.cpp
    1) Класс Constant разделила на 2: Constant + PrimeNumberHelper
    2) Изменила названия функций
    3) int64_t -> size_t

--- f2.cpp
+++ f2.cpp
@@ -5,39 +5,47 @@
 
 class Constant {
  public:
-  static const int64_t kN = 5e6 + 2;
-  static bool np[kN];
-  static int64_t prime[kN];
-  static int64_t pi[kN];
-  static const int64_t kM = 7;
-  static const int64_t kPM = 2 * 3 * 5 * 7 * 11 * 13 * 17;
-  static int64_t phi[kPM + 1][kM + 1];
-  static int64_t sz[kM + 1];
+  Constant() = default;
+  
+  static const size_t kN = 5e6 + 2;
+  static const size_t kM = 7;
+  static const size_t kPM = 2 * 3 * 5 * 7 * 11 * 13 * 17;
 };
 
-const int64_t Constant::kN;
-bool Constant::np[Constant::kN];
-int64_t Constant::prime[Constant::kN];
-int64_t Constant::pi[Constant::kN];
-const int64_t Constant::kM;
-const int64_t Constant::kPM;
-int64_t Constant::phi[Constant::kPM + 1][Constant::kM + 1];
-int64_t Constant::sz[Constant::kM + 1];
+class PrimeNumberHelper {
+ public:
+   PrimeNumberHelper() = default;
 
-int64_t GetPrime() {
-  int64_t cnt = 0;
-  Constant::np[0] = Constant::np[1] = true;
-  Constant::pi[0] = Constant::pi[1] = 0;
-  for (int64_t i = 2; i < Constant::kN; ++i) {
-    if (!Constant::np[i]) {
-      Constant::prime[++cnt] = i;
+   static bool np[Constant::kN];
+   static size_t prime[Constant::kN];
+   static size_t pi[Constant::kN];
+   static size_t phi[Constant::kPM + 1][Constant::kM + 1];
+   static size_t sz[Constant::kM + 1];
+};
+
+// const size_t Constant::kN;
+// bool prime_number_helper.np[Constant::kN];
+// size_t prime_number_helper.prime[Constant::kN];
+// size_t prime_number_helper.pi[Constant::kN];
+// const size_t Constant::kM;
+// const size_t Constant::kPM;
+// size_t prime_number_helper.phi[Constant::kPM + 1][Constant::kM + 1];
+// size_t prime_number_helper.sz[Constant::kM + 1];
+
+size_t GeneratePrimes(PrimeNumberHelper& prime_number_helper, Constant& constant) {
+  size_t cnt = 0;
+  prime_number_helper.np[0] = prime_number_helper.np[1] = true;
+  prime_number_helper.pi[0] = prime_number_helper.pi[1] = 0;
+  for (size_t i = 2; i < constant.kN; ++i) {
+    if (!prime_number_helper.np[i]) {
+      prime_number_helper.prime[++cnt] = i;
     }
-    Constant::pi[i] = cnt;
+    prime_number_helper.pi[i] = cnt;
 
-    for (int64_t j = 1; j <= cnt && i * Constant::prime[j] < Constant::kN;
+    for (size_t j = 1; j <= cnt && i * prime_number_helper.prime[j] < constant.kN;
          ++j) {
-      Constant::np[i * Constant::prime[j]] = true;
-      if (i % Constant::prime[j] == 0) {
+      prime_number_helper.np[i * prime_number_helper.prime[j]] = true;
+      if (i % prime_number_helper.prime[j] == 0) {
         break;
       }
     }
@@ -45,30 +53,15 @@ int64_t GetPrime() {
   return cnt;
 }
 
-void Init() {
-  GetPrime();
-  Constant::sz[0] = 1;
-  for (int64_t i = 0; i <= Constant::kPM; ++i) {
-    Constant::phi[i][0] = i;
-  }
-  for (int64_t i = 1; i <= Constant::kM; ++i) {
-    Constant::sz[i] = Constant::prime[i] * Constant::sz[i - 1];
-    for (int64_t j = 1; j <= Constant::kPM; ++j) {
-      Constant::phi[j][i] = Constant::phi[j][i - 1] -
-                            Constant::phi[j / Constant::prime[i]][i - 1];
-    }
-  }
-}
-
-int64_t SqrtUniversal(int64_t x, int64_t degree) {
-  int64_t r = 0;
+size_t SqrtUniversal(size_t x, size_t degree) {
+  size_t r = 0;
   if (degree == 2) {
-    r = static_cast<int64_t>(sqrt(static_cast<double>(x) - 0.1));
+    r = static_cast<size_t>(sqrt(static_cast<double>(x) - 0.1));
     while (r * r <= x) {
       ++r;
     }
   } else {
-    r = static_cast<int64_t>(cbrt(static_cast<double>(x) - 0.1));
+    r = static_cast<size_t>(cbrt(static_cast<double>(x) - 0.1));
     while (r * r * r <= x) {
       ++r;
     }
@@ -76,71 +69,89 @@ int64_t SqrtUniversal(int64_t x, int64_t degree) {
   return r - 1;
 }
 
-int64_t GetPhi(int64_t x, int64_t s) {
+size_t CountNumbersWithKPrimeFactors(size_t x, size_t s, PrimeNumberHelper& prime_number_helper, Constant& constant) {
   if (s == 0) {
     return x;
   }
-  if (s <= Constant::kM) {
-    return Constant::phi[x % Constant::sz[s]][s] +
-           (x / Constant::sz[s]) * Constant::phi[Constant::sz[s]][s];
+  if (s <= constant.kM) {
+    return prime_number_helper.phi[x % prime_number_helper.sz[s]][s] +
+           (x / prime_number_helper.sz[s]) * prime_number_helper.phi[prime_number_helper.sz[s]][s];
   }
-  if (x <= Constant::prime[s] * Constant::prime[s]) {
-    return Constant::pi[x] - s + 1;
+  if (x <= prime_number_helper.prime[s] * prime_number_helper.prime[s]) {
+    return prime_number_helper.pi[x] - s + 1;
   }
-  if (x <= Constant::prime[s] * Constant::prime[s] * Constant::prime[s] &&
-      x < Constant::kN) {
-    int64_t s2x = Constant::pi[SqrtUniversal(x, 2)];
-    int64_t ans = Constant::pi[x] - (s2x + s - 2) * (s2x - s + 1) / 2;
-    for (int64_t i = s + 1; i <= s2x; ++i) {
-      ans += Constant::pi[x / Constant::prime[i]];
+  if (x <= prime_number_helper.prime[s] * prime_number_helper.prime[s] * prime_number_helper.prime[s] &&
+      x < constant.kN) {
+    size_t s2x = prime_number_helper.pi[SqrtUniversal(x, 2)];
+    size_t ans = prime_number_helper.pi[x] - (s2x + s - 2) * (s2x - s + 1) / 2;
+    for (size_t i = s + 1; i <= s2x; ++i) {
+      ans += prime_number_helper.pi[x / prime_number_helper.prime[i]];
     }
     return ans;
   }
 
-  return GetPhi(x, s - 1) - GetPhi(x / Constant::prime[s], s - 1);
+  return CountNumbersWithKPrimeFactors(x, s - 1, prime_number_helper, constant) - CountNumbersWithKPrimeFactors(x / prime_number_helper.prime[s], s - 1, prime_number_helper, constant);
 }
 
-int64_t GetPi(int64_t x) {
-  if (x < Constant::kN) {
-    return Constant::pi[x];
+size_t CountPrimesPiFunc(size_t x, PrimeNumberHelper& prime_number_helper, Constant& constant) {
+  if (x < constant.kN) {
+    return prime_number_helper.pi[x];
   }
-  int64_t ans = GetPhi(x, Constant::pi[SqrtUniversal(x, 3)]) +
-                Constant::pi[SqrtUniversal(x, 3)] - 1;
-  for (int64_t i = Constant::pi[SqrtUniversal(x, 3)] + 1,
-               ed = Constant::pi[SqrtUniversal(x, 2)];
+  size_t ans = CountNumbersWithKPrimeFactors(x, prime_number_helper.pi[SqrtUniversal(x, 3)], prime_number_helper, constant) +
+                prime_number_helper.pi[SqrtUniversal(x, 3)] - 1;
+  for (size_t i = prime_number_helper.pi[SqrtUniversal(x, 3)] + 1,
+               ed = prime_number_helper.pi[SqrtUniversal(x, 2)];
        i <= ed; ++i) {
-    ans -= GetPi(x / Constant::prime[i]) - i + 1;
+    ans -= CountPrimesPiFunc(x / prime_number_helper.prime[i], prime_number_helper, constant) - i + 1;
   }
   return ans;
 }
 
-int64_t LehmerPi(int64_t x) {
-  Init();
-  if (x < Constant::kN) {
-    return Constant::pi[x];
+size_t CountPrimes(size_t x, PrimeNumberHelper& prime_number_helper, Constant& constant) {
+  if (x < constant.kN) {
+    return prime_number_helper.pi[x];
   }
-  int64_t a = LehmerPi(SqrtUniversal(SqrtUniversal(x, 2), 2));
-  int64_t b = LehmerPi(SqrtUniversal(x, 2));
-  int64_t c = LehmerPi(SqrtUniversal(x, 3));
-  int64_t sum =
-      GetPhi(x, a) + static_cast<int64_t>(b + a - 2) * (b - a + 1) / 2;
-  for (int64_t i = a + 1; i <= b; i++) {
-    int64_t w = x / Constant::prime[i];
-    sum -= LehmerPi(w);
+  size_t a = CountPrimes(SqrtUniversal(SqrtUniversal(x, 2), 2), prime_number_helper, constant);
+  size_t b = CountPrimes(SqrtUniversal(x, 2), prime_number_helper, constant);
+  size_t c = CountPrimes(SqrtUniversal(x, 3), prime_number_helper, constant);
+  size_t sum =
+      CountNumbersWithKPrimeFactors(x, a, prime_number_helper, constant) + static_cast<size_t>(b + a - 2) * (b - a + 1) / 2;
+  for (size_t i = a + 1; i <= b; i++) {
+    size_t w = x / prime_number_helper.prime[i];
+    sum -= CountPrimes(w, prime_number_helper, constant);
     if (i > c) {
       continue;
     }
-    int64_t lim = LehmerPi(SqrtUniversal(w, 2));
-    for (int64_t j = i; j <= lim; j++) {
-      sum -= LehmerPi(w / Constant::prime[j]) - (j - 1);
+    size_t lim = CountPrimes(SqrtUniversal(w, 2), prime_number_helper, constant);
+    for (size_t j = i; j <= lim; j++) {
+      sum -= CountPrimes(w / prime_number_helper.prime[j], prime_number_helper, constant) - (j - 1);
     }
   }
   return sum;
 }
 
 int main() {
-  int64_t n = 0;
+
+  size_t n = 0;
   std::cin >> n;
-  std::cout << LehmerPi(n);
+
+  Constant constant;
+  PrimeNumberHelper prime_number_helper;
+
+  GeneratePrimes(prime_number_helper, constant);
+
+  prime_number_helper.sz[0] = 1;
+  for (size_t i = 0; i <= constant.kPM; ++i) {
+    prime_number_helper.phi[i][0] = i;
+  }
+  for (size_t i = 1; i <= constant.kM; ++i) {
+    prime_number_helper.sz[i] = prime_number_helper.prime[i] * prime_number_helper.sz[i - 1];
+    for (size_t j = 1; j <= constant.kPM; ++j) {
+      prime_number_helper.phi[j][i] = prime_number_helper.phi[j][i - 1] -
+                            prime_number_helper.phi[j / prime_number_helper.prime[i]][i - 1];
+    }
+  }
+
+  std::cout << CountPrimes(n, prime_number_helper, constant);
   return 0;
 }
